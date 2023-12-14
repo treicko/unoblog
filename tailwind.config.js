32ed29dee32bdba5ca1d94ffa7538e1c035b323f
Leonardo Mejia <leo_mejia_p@hotmail.com>
2023-12-14 02:35:12 -0400

    Added tailwind and mongoDB dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+// "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
